67eea81d2aa90f34f3a40bd10c9821d601ecde6d
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-06-07 22:52:13 +0530

    changing signin and signup pages

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -2,6 +2,7 @@
 import { SignIn } from "@clerk/nextjs";
 import "../../../styles/globals.css";
 import Image from "next/image";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
@@ -24,36 +25,18 @@ export default function Home() {
       </div>
       <div className="container relative hidden h-[750px] items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            SiteGenie
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
+          <div className="absolute inset-0 bg-indigo-700" />
+          <div className="relative z-20 flex flex-col space-y-6">
+            <div className="text-5xl font-bold">SiteGenie</div>
+            <div className="flex items-center text-lg">
+              Try our service by Signing Up <ArrowRight className="ml-2" />
+            </div>
           </div>
         </div>
+
         <div className="flex items-center justify-center lg:p-8">
           <div className="w-full max-w-md mx-auto flex flex-col justify-center space-y-6">
-            <SignIn />
+            <SignIn className="bg-indigo-600 text-white p-2 rounded-md hover:bg-indigo-700" />
           </div>
         </div>
       </div>
