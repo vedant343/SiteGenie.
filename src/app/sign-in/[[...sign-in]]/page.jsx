37ae30509b7d82e6fcab82ecac7e4fbb7286cf06
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-06-07 23:02:20 +0530

    signin signup done

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -7,36 +7,32 @@ import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
       <div className="container relative hidden h-[750px] items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-indigo-700" />
           <div className="relative z-20 flex flex-col space-y-6">
             <div className="text-5xl font-bold">SiteGenie</div>
             <div className="flex items-center text-lg">
-              Try our service by Signing Up <ArrowRight className="ml-2" />
+              Try out our service by signing inn <ArrowRight className="ml-2" />
             </div>
           </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;Some people call this artificial intelligence, but the
+                reality is this technology will enhance us. So instead of
+                artificial intelligence, I think we'll augment our
+                intelligence.&rdquo;
+              </p>
+              <footer className="text-sm">
+                – Ginni Rometty, Former CEO of IBM
+              </footer>
+            </blockquote>
+          </div>
         </div>
-
         <div className="flex items-center justify-center lg:p-8">
           <div className="w-full max-w-md mx-auto flex flex-col justify-center space-y-6">
-            <SignIn className="bg-indigo-600 text-white p-2 rounded-md hover:bg-indigo-700" />
+            <SignIn />
           </div>
         </div>
       </div>
